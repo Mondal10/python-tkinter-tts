aa7cefa2180826ac403b1157f2d155b3937c28b3
Mondal10 <amitmondal111.am@gmail.com>
2020-01-01 15:06:40 +0530

    audio file generation and playback

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,21 +1,11 @@
-import tkinter as tk
-from gtts import gTTS
-from playsound import playsound
+import tkinter as tk  # To create GUI
+from gtts import gTTS  # Google Text To Speech
+from playsound import playsound  # To play the audio
 
 # Creating the UI window
 ui_window = tk.Tk()
 ui_window.title("Text To Speech")
 ui_window.geometry("200x100")
-
-# Functionality
-
-
-def conver_to_speech():
-    text = entry.get()
-    speech = gTTS(text=text, lang="en")
-    speech.save(r'D:Project/Python/python-tts/audio/dummy.mp3')
-    playsound(r'D:Project/Python/python-tts/audio/dummy.mp3')
-
 
 label = tk.Label(ui_window, text="Enter the Text here:")
 label.grid(row=0, column=0)
@@ -23,7 +13,17 @@ label.grid(row=0, column=0)
 entry = tk.Entry(ui_window)
 entry.grid(row=1, column=0)
 
-button = tk.Button(ui_window, text="Go", command=conver_to_speech)
+
+def convert_to_speech():
+    # Functionality to convert text to speech
+    text = entry.get()
+    speech = gTTS(text=text, lang="en")
+    # Make sure audio folder is already present or else it will give error
+    speech.save('./audio/test.mp3')
+    playsound('./audio/test.mp3')
+
+
+button = tk.Button(ui_window, text="Go", command=convert_to_speech)
 button.grid(row=1, column=1)
 
 # `mainloop()` is infinite loop used to run the application,
