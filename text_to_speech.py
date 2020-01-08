55e610259bb0373f0fd14c15b1ba57b494b51b36
Mondal10 <amitmondal111.am@gmail.com>
2020-01-08 17:55:35 +0530

    language dropdown added and structuring code

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,7 +1,34 @@
 import tkinter as tk  # To create GUI
 import time  # Used for time stamp on audio file
 from gtts import gTTS  # Google Text To Speech
+from gtts import lang  # Languages available in gtts
 from playsound import playsound  # To play the audio
+
+# function to return key for any value 
+def get_key(val):
+    for key, value in languages.items():
+        if val == value:
+            return key
+
+    return "Key doesn't exist"
+
+def convert_to_speech():
+    # Functionality to convert text to speech
+    if not entry.get():
+        # If nothing is entered in textbox then return
+        return False
+    text = entry.get()
+    seleced_language = defaultSelect.get()
+
+    lang = get_key(seleced_language) # For different accent audio
+    speech = gTTS(text=text, lang=lang)
+
+    time_stamp = time.strftime("%Y%m%d_%H%M%S")
+    # Make sure audio folder is already present or else it will give error
+    generate_file_name = f'./audio/audio_{lang}_{time_stamp}.mp3'
+
+    speech.save(generate_file_name)
+    playsound(generate_file_name)
 
 # Creating the UI window
 ui_window = tk.Tk()
@@ -14,22 +41,13 @@ label.grid(row=0, column=0)
 entry = tk.Entry(ui_window)
 entry.grid(row=1, column=0)
 
+languages = lang.tts_langs()
 
-def convert_to_speech():
-    # Functionality to convert text to speech
-    if not entry.get():
-        # If nothing is entered in textbox then return
-        return False
-    text = entry.get()
-    speech = gTTS(text=text, lang="en")
-
-    time_stamp = time.strftime("%Y%m%d_%H%M%S")
-    # Make sure audio folder is already present or else it will give error
-    generate_file_name = f'./audio/audio_{time_stamp}.mp3'
-
-    speech.save(generate_file_name)
-    playsound(generate_file_name)
-
+defaultSelect = tk.StringVar(ui_window)
+defaultSelect.set(languages['en'])
+options = languages.values()
+dropdown = tk.OptionMenu(ui_window, defaultSelect, *options)
+dropdown.grid(row=2, column=0)
 
 button = tk.Button(ui_window, text="Convert", command=convert_to_speech)
 button.grid(row=1, column=1)
